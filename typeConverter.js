3710fcbcb5cc913f0851fe4c5132e93e41934718
Anut.pri <124768336+anutpri@users.noreply.github.com>
2023-03-10 18:03:16 +0000

    created typeConverter.js

---
+++ typeConverter.js
@@ -0,0 +1,57 @@
+
+let cfalse = false;
+let ctrue = true;
+let c0 = 0;
+let c1 = 1;
+let C0 = "0";
+let c000 = "000";
+let C1 = "1";
+let cNaN = NaN;
+let cInfinity = Infinity;
+let CInfinity = -Infinity;
+let c = "";
+let c20 = "20";
+let cTwenty = "Twenty";
+let cnull = null;
+let cundefinited;
+
+
+
+console.log("Convert to Number = "+ Number(cfalse), Number(ctrue), Number(c0), Number(c1), Number(C0), Number(c000), Number(C1), Number(cNaN), Number(cInfinity), Number(CInfinity), Number(c), Number(c20), Number(cTwenty), Number(cnull), Number(cundefinited));
+console.log("Convert to String = "+ String(cfalse), String(ctrue), String(c0), String(c1), String(C0), String(c000), String(C1), String(cNaN), String(cInfinity), String(CInfinity), String(c), String(c20), String(cTwenty), String(cnull), String(cundefinited));
+console.log("Convert to Boolean = "+ Boolean(cfalse), Boolean(ctrue), Boolean(c0), Boolean(c1), Boolean(C0), Boolean(c000), Boolean(C1), Boolean(cNaN), Boolean(cInfinity), Boolean(CInfinity), Boolean(c), Boolean(c20), Boolean(cTwenty), Boolean(cnull), Boolean(cundefinited));
+
+
+// let convertValue = ['false', 'true', '0', '1', "0"];
+// // let [num1, num2, num3, num4, num5] = convertValue.map(Boolean);
+// // console.log(convertValue[0]);
+// // console.log(`Convert to Boolean = ${num1}`);
+// let cNumber = convertValue.map(Number);
+// let cBoolean = convertValue.map(Boolean);
+// console.log(cNumber);
+// console.log(cBoolean);
+
+// console.log(`Convert to Number = ${number}`);
+
+// let test = {
+//     a:false,
+//     b:true,
+//     c:0,
+//     d:1,
+
+// }
+// let number1 = Number(test);
+// console.log(test.b);
+
+
+
+// let cNumber = {
+//  1: false,
+//  2: true,
+// }
+// // let cNumber = Number(cfalse);
+// let cBoolean = Boolean(cfalse);
+// let cString = String(cfalse)
+// let convertNum = Number(false; true, 0, 1); 
+
+// console.log(convertNum);
