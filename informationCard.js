609f2dde4bb99e931871a76915e70fa4a5ea2be4
Anut.pri <124768336+anutpri@users.noreply.github.com>
2023-03-10 18:15:48 +0000

    created informationCard.js

---
+++ informationCard.js
@@ -0,0 +1,6 @@
+let myName = "Nick";
+let myBirthPlace = "Khonkean";
+let myFavorite = "Anime";
+
+console.log(`Hello my name is ${myName}, 
+I was born in ${myBirthPlace} and I my favorite is ${myFavorite}`);
